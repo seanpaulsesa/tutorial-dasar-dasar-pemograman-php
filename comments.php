47f6938d7e91cf068a10d830a0f401c759148aac
seanpaulsesa <pauluspaul7700@gmail.com>
2023-03-19 01:57:15 +0900

    tambah comments.php

---
+++ comments.php
@@ -0,0 +1,14 @@
+<?php
+// contoh komentar single-line di PHP
+
+#  Ini juga salah satu komentar single-line di php
+
+/*
+   Kalau yang  ini merupakan contoh 
+   komentar multiple-lines
+   yang bisah diterapkan 
+   di dalam sebuah file PHP
+*/
+
+  echo "PHP Comments";
+?>
