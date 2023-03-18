fa625899263a77a247c293657e8bef0035cf9866
seanpaulsesa <pauluspaul7700@gmail.com>
2023-03-19 01:47:36 +0900

    tambah php-index.php

---
+++ php-html.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kombinasi PHP & HTML</title>
+</head>
+<body>
+    
+
+    <h1>Kombinasi PHP & HTML</h1>
+    <?php
+        echo "Hello PHP!";
+    ?>
+
+</body>
+</html>
