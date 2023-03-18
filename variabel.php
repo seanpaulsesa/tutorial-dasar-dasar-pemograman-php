9e79d147790010f829e59b61f0819e909806d451
seanpaulsesa <pauluspaul7700@gmail.com>
2023-03-19 01:54:48 +0900

    tambah variabel.php

---
+++ variabel.php
@@ -0,0 +1,17 @@
+<?php
+
+    // contoh penulisan variables dalam file PHP
+    $title      = "SaCode";
+    $tag_line   = "Papua Coding School";
+    $tahun      = "2023";
+
+    // Menampilkan data dari dalam variables
+    echo $title;
+    echo "<br>";
+
+    echo $tag_line;
+    echo "<br>";
+    
+    echo $tahun;
+
+?>
